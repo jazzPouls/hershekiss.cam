24b220345392b201240e664c56e24f0482439bd4
jazzPouls <jzpouls@gmail.com>
2024-11-21 03:40:42 -0500

    back to before

--- lib/cli.ts
+++
@@ -1,5 +0,0 @@
-import { Octokit } from 'octokit'
-
-const cli = new Octokit({ auth: process.env.ACCESS_TOKEN })
-
-export default cli
--- next.config.js
+++ next.config.js
@@ -1,28 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['avatars.githubusercontent.com', 'raw.githubusercontent.com'],
-    unoptimized: true,
-  },
-  env: {
-    OWNER: process.env.OWNER,
-    REPO: process.env.REPO,
-    LABELS: 'published',
+  reactStrictMode: true
+};
 
-    // config
-    comment: 'true', // true or false
-    theme: 'both', // 'light' or 'dark' or 'both'
-    rss: 'true', // true or false
-    back2top: 'true', // true or false
-    mail: 'liuyihan0410@gmail.com', // your email
-    twitter: '', // your twitter
-    bio: '',
-  },
-  basePath: process.env.DEPLOY_TARGET === 'gh-pages' && process.env.REPO !== `${process.env.OWNER}.github.io`
-    ? `/${process.env.REPO}`
-    : '',
-}
-
-// eslint-disable-next-line unicorn/prefer-module
-module.exports = nextConfig
+module.exports = nextConfig;
